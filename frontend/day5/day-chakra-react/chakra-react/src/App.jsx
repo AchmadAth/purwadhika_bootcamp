d21475511ec666b102b77f3e8196ff394250bddf
Atho <ahmadathoillah28@gmail.com>
2023-11-21 22:25:12 +0700

    done

---
+++ frontend/day5/day-chakra-react/chakra-react/src/App.jsx
@@ -0,0 +1,95 @@
+import {
+  Box,
+  Center,
+  Heading,
+  Button,
+  Text,
+  Divider,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import "./App.css";
+import { Checkbox } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+
+function App() {
+  return (
+    <>
+      <Box bgColor={"#1C1F2D"}>
+        <Box>
+          <Center>
+            <Text fontSize="3xl" color={"white"}>
+              Chores ToDo List
+            </Text>
+          </Center>
+        </Box>
+        <Box>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Center>
+            <Checkbox padding={"10px"}></Checkbox>
+            <Text padding={"10px"} paddingRight={"25px"} color={"white"}>
+              Create Guest Experience mobile check-in
+            </Text>
+            <DeleteIcon color={"red"} />
+          </Center>
+          <Divider orientation="horizontal" />
+          <Center>
+            <Text fontSize={"30px"} color={"white"}>
+              Done : 0
+            </Text>
+          </Center>
+          <Center>
+            <VStack alignItems={"flex-start"}>
+              <Text color={"white"}>Add Todo</Text>
+              <Input width={"500px"} />
+              <Button>Add Task</Button>
+            </VStack>
+          </Center>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default App;
